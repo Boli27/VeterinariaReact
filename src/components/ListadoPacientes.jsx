8e66d027a6e07c4475efcd4d7ac74b53944cf2c8
Boli27 <105827627+Boli27@users.noreply.github.com>
2023-08-30 20:32:48 -0500

    Creacion de los componentes ListadoPacientes y Pacientes

---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,16 @@
+import Pacientes from "./Pacientes"
+
+const ListadoPacientes = () => {
+    return(
+        <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
+            <h2 className="font-black text-3xl text-center">Listado pacientes</h2>
+            <p className="text-xl mt-5 mb-10 text-center">Administra tus 
+            <span className="text-indigo-600 font-bold">pacients y citas</span> </p>
+
+            <Pacientes/>
+        </div>
+
+    )
+}
+
+export default ListadoPacientes
---
+++ src/components/Pacientes.jsx
@@ -0,0 +1,16 @@
+
+
+const Pacientes = () => {
+    return(
+        <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+            <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: <span className="font-normal normal-case">Fabricio</span></p>
+            <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: <span className="font-normal normal-case">Boli</span></p>
+            <p className="font-bold mb-3 text-gray-700 uppercase">Email: <span className="font-normal normal-case">Tuki@gmail.com</span></p>
+            <p className="font-bold mb-3 text-gray-700 uppercase">Fecha alta: <span className="font-normal normal-case">30/08/2023</span></p>
+            <p className="font-bold mb-3 text-gray-700 uppercase">Sintomas: <span className="font-normal normal-case">Ta malito</span></p>
+        </div>
+
+    )
+}
+
+export default Pacientes
