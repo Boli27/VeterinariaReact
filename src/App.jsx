d7162c85087751aab056f8a60b4f95b6f9980d8a
Boli27 <105827627+Boli27@users.noreply.github.com>
2023-08-30 20:30:24 -0500

    Creacion de los componentes formulario y header

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Formulario from "./components/Formulario"
+import Header from "./components/Header"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      
-    </>
+    <div className="container mx-auto mt-20">
+      <Header />
+
+      <div className="mt-12 md:flex">
+        <Formulario />
+
+      </div>
+
+    </div>
   )
 }
 
