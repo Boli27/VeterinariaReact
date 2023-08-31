9b2527876ddda23992acc5803eb301add5bbb798
Boli27 <105827627+Boli27@users.noreply.github.com>
2023-08-30 20:39:56 -0500

    Correccion de estilos con tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["index.html", "./src/**/.jsx"],
+  content: [
+    "./index.html", 
+    "./src/**/*.jsx"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
 
