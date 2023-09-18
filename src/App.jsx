083b869ab671f84036d3a92244dde640763305b0
Boli <judiemu27@gmai.com>
2023-09-18 17:55:50 -0500

    Creacion de la funcionalidad eliminar y actualizacion de todo el proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,34 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import ListadoPacientes from "./components/ListadoPacientes"
 import Formulario from "./components/Formulario"
 import Header from "./components/Header"
-import ListadoPacientes from "./components/ListadoPacientes"
 
 function App() {
 
-  const[pacientes,setPacientes]= useState(
-    [
-      {
-        nombre:"Boli",
-        propietario:"Yo",
-        email:"dad",
-        fecha:"asdas",
-        sintomas:"sdasd"
-      }
-    ]
-    )
+  const [pacientes, setPacientes] = useState(
+    JSON.parse(localStorage.getItem("pacientes")) || []
+  )
+
+  const guardar = () => {
+    localStorage.setItem("pacientes", JSON.stringify(pacientes))
+  }
+
+  const borrarLocal = ()=>{
+    localStorage.removeItem("pacientes")
+    setPacientes([]);
+  }
+
+  useEffect(() => {
+    guardar()
+  }, [pacientes])
 
   return (
     <div className="container mx-auto mt-20">
       <Header />
 
       <div className="mt-12 md:flex">
-        <Formulario pacientes={pacientes} setPacientes={setPacientes}/>
-        <ListadoPacientes pacientes={pacientes}/>
+        <Formulario pacientes={pacientes} setPacientes={setPacientes} borrarLocal={borrarLocal}/>
+        <ListadoPacientes pacientes={pacientes} setPacientes={setPacientes}/>
       </div>
 
     </div>
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,37 +1,30 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import Alerta from "./Alerta";
-const Formulario = ({ pacientes, setPacientes }) => {
 
+const Formulario = ({ pacientes, setPacientes, borrarLocal}) => {
 
     const [nombre, setNombre] = useState("");
-    const [propietario, setNombrepropietario] = useState("");
+    const [propietario, setPropietario] = useState("");
     const [email, setEmail] = useState("");
-    const [fecha, setFecha] = useState("");
+    const [alta, setAlta] = useState("");
     const [sintomas, setSintomas] = useState("");
-    const [alerta, setAlerta] = useState({});
+    const [alerta, setAlerta] = useState({})
 
-    // principales hooks que aprender 
-    // useState, useEffect, useMemo = useCallback, useReducer, useRef
-    useEffect(() => {
-        console.log(1)
-    }, [])
-    //variable creada como objeto o algo asi
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
-    //eventos unicos de react, en este caso una alerta el cual ubicaremos en el formulario para enviar.
-    //aqui hacemos uso de la alerta que creamos en nuestro componente Alertar.
-    const handlesubmit = (e) => {
-        e.preventDefault(); //funcionalidad para que la pagina no se reestablesca
-
-        if ([nombre, propietario, email, fecha, sintomas].includes("")) {
+        if ([nombre, propietario, email, alta, sintomas].includes("")) {
             return setAlerta({
                 error: true,
-                mensaje: "Tienes que llenar todos los campos"
+                mensaje: "Debes llenar todos los campos"
             })
         }
+
         setAlerta({
             error: false,
             mensaje: "Paciente registrado"
         })
+
         setPacientes(
             [
                 ...pacientes,
@@ -39,21 +32,21 @@ const Formulario = ({ pacientes, setPacientes }) => {
                     nombre,
                     propietario,
                     email,
-                    fecha,
+                    alta,
                     sintomas
                 }
             ]
-
         )
-        //forma para hacer que el formulario quede vacion posterior al envio de uno
+
         setNombre("")
-        setNombrepropietario("")
+        setPropietario("")
         setEmail("")
-        setFecha("")
+        setAlta("")
         setSintomas("")
     }
 
     const { mensaje } = alerta;
+
     return (
         <div className="md:w-1/2 lg:w-2/5">
             <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
@@ -61,58 +54,46 @@ const Formulario = ({ pacientes, setPacientes }) => {
             <p className="text-lg mt-5 text-center mb-5">Añade Pacientes y <span className="text-indigo-600 font-bold">Administralos</span></p>
 
             <form className="bg-white shadow-sm rounded-lg py-10 px-5"
-                onSubmit={handlesubmit}//instancia en la que llamamos la alerta de enviar para el formulario
+                onSubmit={handleSubmit}
             >
-                {mensaje && (<Alerta error={alerta.error} mensaje={mensaje} />
-                )}
+
+                {mensaje && (<Alerta error={alerta.error} mensaje={mensaje} />)}
 
                 <div className="mb-5">
                     <label htmlFor="nombre" className="block text-gray-700 uppercase font-bold">Nombre Mascota</label>
-                    <input type="text" className="shadow-lg border-2 w-full p-2 mt-2 placeholder-gray-500 rounded-md" placeholder="Nombre de la Mascota"
-                        name="nombre" id="nombre" onChange={
-                            e => {
-                                setNombre(e.target.value)
-                            }}/*Este ultimo fragmento nos permte ingresar informacion a la constante de nombre*/ value={nombre} />
+                    <input type="text" className="shadow-lg border-2 w-full p-2 mt-2 placeholder-gray-500 rounded-md" placeholder="Nombre de la Mascota" name="nombre" id="nombre"
+                        onChange={(e) => {
+                            setNombre(e.target.value)
+                        }
+                        }
+                        value={nombre} />
                 </div>
 
                 <div className="mb-5">
                     <label htmlFor="propietario" className="block text-gray-700 uppercase font-bold">Nombre Propietario</label>
-                    <input type="text" className="shadow-lg border-2 w-full p-2 mt-2 placeholder-gray-500 rounded-md" placeholder="Nombre del Propietario"
-                        name="propietario" id="propietario" onChange={
-                            e => {
-                                setNombrepropietario(e.target.value)
-                            }} value={propietario} />
+                    <input type="text" className="shadow-lg border-2 w-full p-2 mt-2 placeholder-gray-500 rounded-md" placeholder="Nombre del Propietario" name="propietario" id="propietario" onChange={e => { setPropietario(e.target.value) }} value={propietario} />
                 </div>
 
                 <div className="mb-5">
                     <label htmlFor="email" className="block text-gray-700 uppercase font-bold">Email Propietario</label>
-                    <input type="email" className="shadow-lg border-2 w-full p-2 mt-2 placeholder-gray-500 rounded-md" placeholder="Email Propietario"
-                        name="email" id="email" onChange={
-                            e => {
-                                setEmail(e.target.value)
-                            }} value={email} />
+                    <input type="email" className="shadow-lg border-2 w-full p-2 mt-2 placeholder-gray-500 rounded-md" placeholder="Email Propietario" name="email" id="email" value={email}
+                        onChange={e => setEmail(e.target.value)} />
                 </div>
 
                 <div className="mb-5">
                     <label htmlFor="fecha_alta" className="block text-gray-700 uppercase font-bold">Fecha Alta</label>
-                    <input type="date" className="shadow-lg border-2 w-full p-2 mt-2 placeholder-gray-500 rounded-md"
-                        name="fecha_alta" id="fecha_alta" onChange={
-                            e => {
-                                setFecha(e.target.value)
-                            }} value={fecha} />
+                    <input type="date" className="shadow-lg border-2 w-full p-2 mt-2 placeholder-gray-500 rounded-md" name="fecha_alta" id="fecha_alta" value={alta} onChange={e => { setAlta(e.target.value) }} />
                 </div>
 
                 <div className="mb-5">
                     <label htmlFor="sintomas" className="block text-gray-700 uppercase font-bold">Sintomas</label>
-                    <textarea className="shadow-lg border-2 w-full p-2 mt-2 placeholder-gray-500 rounded-md" placeholder="Sintomas de la Mascota"
-                        name="sintomas" id="sintomas" onChange={
-                            e => {
-                                setSintomas(e.target.value)
-                            }} value={sintomas} />
+                    <textarea className="shadow-lg border-2 w-full p-2 mt-2 placeholder-gray-500 rounded-md" placeholder="Sintomas de la Mascota" name="sintomas" id="sintomas" value={sintomas} onChange={e => setSintomas(e.target.value)} />
                 </div>
 
                 <input type="submit" className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 hover:cursor-pointer transition-all" value="Agregar Paciente" />
 
+
+                <input type="button" value="Borrar Todo" onClick={borrarLocal} />
             </form>
 
         </div>
